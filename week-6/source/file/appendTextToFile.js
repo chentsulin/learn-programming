635b5a90273f8a5534c9602da40b8607fbbe7d4d
C.T. Lin <chentsulin@gmail.com>
2015-11-16 21:17:53 +0800

    fix i

--- week-6/source/file/appendTextToFile.js
+++ week-6/source/file/appendTextToFile.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 
-fs.appendFile('resource/file1.txt', i + '. Annie\nIris\n', function (err) {
+fs.appendFile('resource/file1.txt', 'Annie\nIris\n', function (err) {
   if (err) {
     console.error(err);
     return;
